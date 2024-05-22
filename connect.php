4442222819825c8644020f94d1c22cf4eff320ad
Farhan Ahmed <160687843+Farhan-Ahmed-Netsmartz@users.noreply.github.com>
2024-05-22 22:21:59 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -7,7 +7,7 @@
 	$number = $_POST['number'];
 
 	// Database connection
-	$conn = new mysqli('localhost','root','','test');
+	$conn = new mysqli('phpconn-server.mysql.database.azure.com','sqygkkdded','phpconn-server.mysql.database.azure.com','test');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
